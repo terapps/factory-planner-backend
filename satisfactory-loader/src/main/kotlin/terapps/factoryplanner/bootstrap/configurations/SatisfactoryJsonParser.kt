5b80f3a19d3adec95ef3bcb95fc3ace55de97fae
Yohan Rimokh <yohan.rimokh@gmail.com>
2024-09-16 21:25:21 +0200

    chore: rename config

--- satisfactory-loader/src/main/kotlin/terapps/factoryplanner/bootstrap/configurations/SatisfactoryJsonParser.kt
+++ satisfactory-loader/src/main/kotlin/terapps/factoryplanner/bootstrap/configurations/SatisfactoryJsonParser.kt
@@ -1,22 +1,15 @@
 package terapps.factoryplanner.bootstrap.configurations
 
 import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
-import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
 import terapps.factoryplanner.bootstrap.dto.GameCategoryDeserializer
 import terapps.factoryplanner.bootstrap.dto.GameObjectCategory
-import terapps.factoryplanner.bootstrap.dto.SatisfactoryStaticData
-import terapps.factoryplanner.bootstrap.dto.generated.FGRecipe
-import terapps.factoryplanner.bootstrap.dto.generated.FGSchematic
 
 @Component
 class SatisfactoryJsonParser {
